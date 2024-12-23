0a8012efd2a3a3e42a151b343847cd0c27f4c61d
Farhanwick <mrfarhanpubg@gmail.com>
2024-12-24 01:58:21 +0600

    first commit

---
+++ src/page/index.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+import "./index.css";
+
+export default function Main() {
+  return (
+    <div className="main-container">
+      <div className="frame">
+        <div className="frame-1">
+          <div className="h-removebg-preview" />
+          <div className="frame-2">
+            <span className="home">Home</span>
+            <span className="about">About</span>
+            <span className="service">Service</span>
+            <span className="portfolio">Portfolio</span>
+            <span className="blog">Blog</span>
+            <span className="contact">contect</span>
+          </div>
+        </div>
+        <div className="frame-3">
+          <div className="vector" />
+          <span className="download-cv">DOWNLOAD CV</span>
+        </div>
+      </div>
+      <div className="rectangle">
+        <div className="flex-column-a">
+          <span className="about-me">About Me </span>
+          <span className="cool-design-project">
+            I like creating a cool design project
+          </span>
+          <div className="frame-4">
+            <span className="age">Age:</span>
+            <span className="age-value">29</span>
+          </div>
+          <div className="frame-5">
+            <span className="residence">Residence:</span>
+            <span className="residence-value">USA</span>
+          </div>
+          <div className="frame-6">
+            <span className="address">Address:</span>
+            <span className="address-value">Lorem ipsum dolor sit amet</span>
+          </div>
+          <div className="frame-7">
+            <span className="email">E-mail:</span>
+            <span className="email-value">email@example.com</span>
+          </div>
+          <div className="frame-8">
+            <span className="phone">Phone:</span>
+            <span className="phone-number">011254698751</span>
+          </div>
+          <div className="frame-9">
+            <span className="freelance">Freelance:</span>
+            <span className="available">Available</span>
+          </div>
+          <div className="frame-a">
+            <div className="ellipse" />
+            <div className="rectangle-b" />
+            <div className="ellipse-c" />
+          </div>
+          <div className="frame-d">
+            <div className="vector-e" />
+            <span className="download-cv-f">DOWNLOAD CV</span>
+          </div>
+        </div>
+        <div className="flex-column-f">
+          <div className="rectangle-10">
+            <div className="flex-row-c">
+              <div className="group">
+                 <button className="button-50-percent">50%</button>
+              </div>
+              <div className="group-11">
+                  <button className="button-75-percent">75%</button>
+              </div>
+            </div>
+            <div className="flex-row-d">
+              <div className="group-13">
+                 <button className="button-38-percent">38%</button>
+              </div>
+              <div className="group-15">
+                  <button className="button-63-percent">63%</button>
+              </div>
+            </div>
+          </div>
+          <span className="long-text">
+            Lorem ipsum dolor sit amet consectetur. Nibh quis a ut dignissim
+            dolor enim ultricies sit vitae. Turpis lorem libero nisl mattis
+            fermentum. Enim lectus pretium auctor diam morbi vulputate. Nec
+            tortor nunc proin purus.{" "}
+          </span>
+          <span className="karem-marko">
+            Karem
+            <br /> Marko
+          </span>
+        </div>
+      </div>
+      <div className="ellipse-17" />
+      <div className="frame-18">
+        <div className="frame-19">
+          <span className="hello-i-am">HELLO I AM</span>
+          <span className="lorehm-ipsmum">Lorehm ipsmum</span>
+          <span className="lorem-ipsum-dolor">
+            Lorem ipsum dolor sit amet consectetur. Mollis feugiat .
+          </span>
+        </div>
+        <div className="frame-1a">
+          <div className="frame-1b">
+            <span className="start-consulting">START CONSULTING</span>
+          </div>
+          <div className="frame-1c">
+            <div className="ellipse-1d" />
+            <span className="lorem-ipsum-dolor-1e">Lorem ipsum dolor sit</span>
+          </div>
+        </div>
+      </div>
+      <div className="vector-1f" />
+      <div className="vector-20" />
+      <div className="rectangle-21">
+        <span className="k">10K</span>
+        <span className="lorem-ipsum-dolor-22">Lorem ipsum dolor sit</span>
+      </div>
+    </div>
+  );
+}
